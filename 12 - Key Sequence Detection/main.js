f9168804b668c694539c6a7f838efe40a4c9db35
Max Teclav <maks.tetslav@yandex.ru>
2021-06-16 00:07:18 +1000

    feat: finish 12th challenge

---
+++ 12 - Key Sequence Detection/main.js
@@ -0,0 +1,13 @@
+const secretCode = 'corn';
+const pressed = [];
+
+window.addEventListener('keyup', (e) => {
+    console.log(e.key);
+    pressed.push(e.key);
+    pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
+
+    if (pressed.join('').includes(secretCode)) {
+        cornify_add();
+    };
+    console.log(pressed);
+});
